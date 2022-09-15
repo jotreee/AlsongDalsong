8b594bcda190c709f6ab82509754f4056682b840
SSAFY <sskong777@gmail.com>
2022-09-15 16:49:51 +0900

    �2[BE-1] User 모델 생성

--- backend/server/urls.py
+++ backend/server/urls.py
@@ -1,7 +1,7 @@
-"""server URL Configuration
+"""django_social_login URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('dj_rest_auth.urls')),
+    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
