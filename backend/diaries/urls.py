abd5d2641a6e891547a486ebc47defca33fb05c4
Hyoseon Kim <apso123@naver.com>
2022-09-17 15:03:23 +0900

    🌻 [BE-1-2] 일기 CRUD 로직 생성 1

--- backend/diaries/urls.py
+++ backend/diaries/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('html/', views.article_html),
+    path('/', views.create),
+    path('/<int:diary_pk>', views.detail),
+    # path('/<int:diary_id>/decorate', views.decorate),
+    path('/<int:diary_pk>/playlist', views.playlist),
+    path('/bookmark', views.bookmark),
+    # path('/<int:diary_pk>/sticker', views.monthSticker),
+    path('/month/<int:month>/emotion', views.monthEmotion),
+    path('/month/<int:month>', views.monthDiary),
 ]
--- backend/diaries/views.py
+++ backend/diaries/views.py
@@ -1,15 +1,101 @@
-from re import T
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, get_list_or_404
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from .serializers import DiarySerializer
 from .models import Diary
+from diaries import serializers
 
-# Create your views here.
-
-# class DiaryView(viewsets.ModelViewSet):
-#     serializer_class = DiarySerializer
-#     queryset = Diary.objects.all()
-
+# Get: 다이어리 목록 반환
+# Post: 다이어리 작성
+@api_view(['GET', 'POST'])
 def create(request):
+    if request.method == 'GET':
+        diary = Diary.objects.all()
+        serializer = DiarySerializer(diary, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = DiarySerializer(request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# Get: 일기 상세보기
+# Put: 일기 수정
+@api_view(['GET', 'PUT'])
+def detail(request, diary_pk):
+    diary = get_object_or_404(Diary, pk=diary_pk)
+
+    if request.method == 'GET':
+        serializer = DiarySerializer(diary)
+        return Response(serializer.data)
+
+    if request.method == 'PUT':
+        serializer = DiarySerializer(diary, request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+
+#################################
+# # Post: 스티커 부착
+# @api_view(['POST'])
+# def decorate():
+#     pass
+#################################
+
+
+#################################
+# # Get: 일기별 플레이리스트 조회
+# # Post: 일기별 플레이리스트 생성
+# @api_view(['GET', 'POST'])
+# def playlist(request):
+#     if request.method == 'GET':
+#         diary = Diary.objects.all()
+#         serializer = DiarySerializer(diary, many=True)
+#         return Response(serializer.data)
+
+#     if request.method == 'POST':
+#         serializer = DiarySerializer(request.data)
+#         if serializer.is_valid(raise_exception=True):
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#################################
+
+
+#################################
+# Get: 일기별 스티커 조회
+# @api_view(['GET'])
+# def monthSticker():
+#     pass
+#################################
+
+
+# Get: 책갈피 모아보기
+# Post: 책갈피 생성
+@api_view(['GET', 'POST'])
+def bookmark(request):
+    if request.method == 'GET':
+        diary = Diary.objects.all()
+        serializer = DiarySerializer(diary, many=True)
+        return Response(serializer.data)
+
+    if request.method == 'POST':
+        serializer = DiarySerializer(request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+# Get: 월별 일기 감정 조회
+@api_view(['GET'])
+def monthEmotion():
+    pass
+
+
+# Get: 월별 일기 모아보기
+@api_view(['GET'])
+def monthDiary():
     pass
