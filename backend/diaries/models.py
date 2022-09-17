2abd80766800201fc244161fe427d603e7c5a768
Hyoseon Kim <apso123@naver.com>
2022-09-17 15:02:41 +0900

    🌻 [BE-1-1] 일기 모델 생성

--- backend/diaries/models.py
+++ backend/diaries/models.py
@@ -4,27 +4,28 @@ from django.db import models
 
 class Diary(models.Model):
     # user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-    title = models.CharField(max_length=20)
-    context = models.TextField
-    emotion = models.IntegerField
-    created_date = models.DateTimeField(auto_now_add=True)
-    updated_date = models.DateTimeField(auto_now=True)
-    
-    def __str__(self):
-        return self.title
+    title = models.CharField(max_length=50)
+    context = models.TextField()
+    emotion = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
 
 class Image(models.Model):
     diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
     # user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-    image_url = models.TextField
+    image_url = models.TextField()
 
 
-class Sentence(models.Model):
-    sentence = models.CharField(max_length=30)
-    emotion = models.IntegerField
-
-
-class Diary_Sentence(models.Model):
+class Bookmark(models.Model):
+    # user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
     diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
-    sentence = models.ForeignKey("diaries.Sentence", on_delete=models.CASCADE)
+    color = models.CharField(max_length=20)
+
+
+
+# class StickerPack
+# class Sticker
+# class MyStickerPack
+# class DiarySticker
+# class DiaryPlaylist
