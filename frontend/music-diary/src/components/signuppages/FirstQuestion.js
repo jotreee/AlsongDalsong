a9893a862b980cddb692da85d8da96a78c1e0d41
MilanoBeer <tgd5753@gmail.com>
2022-09-20 14:39:52 +0900

    🦁 [FE-2-2] feat : 설문조사 UI 디자인 75%

--- frontend/music-diary/src/components/signuppages/FirstQuestion.js
+++ frontend/music-diary/src/components/signuppages/FirstQuestion.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import "../../css/signuppages/SignupPageBook.css";
+import "../../css/signuppages/QuestionBox.css"
 import Button from "../Common/Button";
 
 import styled from "styled-components";
@@ -14,6 +15,12 @@ const SignupInfoBookcontainer = styled.div`
 `;
 
 function FirstQuestion() {
+    const [firstAnswer, setFirstAnswer] = useState()
+    const [emotionAnswer, setEmotionAnswer] = useState()
+    const [dance, setDance] = useState(false)
+    const [sad, setSad] = useState(false)
+    const [normal, setNormal] = useState(false)
+    const [energytic, setEnergytic] = useState(false)
 
     const navigate = useNavigate()
 
@@ -21,6 +28,41 @@ function FirstQuestion() {
         navigate('/signup/question/two')
     }
 
+    const onClickDance = () => {
+      setDance(!dance)
+      if(!dance){
+        setFirstAnswer("신나는")
+      }else{
+        setFirstAnswer("   ")
+      }
+    }
+
+    const onClickSad = () => {
+      setSad(!sad)
+      if(!sad){
+        setFirstAnswer("슬픈")
+      }else{
+        setFirstAnswer("   ")
+      }
+    }
+
+    const onClickNormal = () => {
+      setNormal(!normal)
+      if(!normal){
+        setFirstAnswer("평온한")
+      }else{
+        setFirstAnswer("   ")
+      }
+    }
+
+    const onClickEnergy = () => {
+      setEnergytic(!energytic)
+      if(!energytic){
+        setFirstAnswer("에너지틱한")
+      }else{
+        setFirstAnswer("   ")
+      }
+    }
 
   return (
     <>
@@ -32,12 +74,27 @@ function FirstQuestion() {
               <div className="page front contents">
                 <div className="intro">
                   <h1>당신의 음악취향은?</h1>
-                  <div className="signup-form-wrapper">
+                  <h2>나는 평소에 "{firstAnswer}" 노래를 듣는다 </h2>
+                  
+                  <div className="first-row">
+                    <div className={dance ? "selected-box" : "question-box"} onClick={onClickDance} >
+                      신나는 노래
+                    </div>
+                    <div className={sad ? "selected-box" : "question-box"} onClick={onClickSad} >
+                      슬픈노래
+                    </div>
+                  </div>
+                  <div className="second-row">
+                    <div className={normal ? "selected-box" : "question-box"} onClick={onClickNormal}>
+                      평온한 노래
+                    </div>
+                    <div className={energytic ? "selected-box" : "question-box"} onClick={onClickEnergy}>
+                      에너지틱한 노래
+                    </div>
+                    </div>
                     <div className="next-btn">
                      <Button name="다음->" color="#AC5050" size="lg" onClick={onMoveQuestionTwo} />
                     </div>
-                  </div>
-         
                 </div>
               </div>
             </div>
