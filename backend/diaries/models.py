433da70813c070d23bfd01f492d850618d87bd92
Hyoseon Kim <apso123@naver.com>
2022-09-21 01:05:11 +0900

    🌻 [BE-2] chore: 일기 모델 변경

--- backend/diaries/models.py
+++ backend/diaries/models.py
@@ -1,11 +1,12 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
 
 class Diary(models.Model):
     # user = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    context = models.TextField()
+    title = models.CharField(max_length=100)
+    content = models.TextField()
     emotion = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -22,6 +23,15 @@ class Bookmark(models.Model):
     diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
     color = models.CharField(max_length=20)
 
+
+# class StickerPack(models.Model):
+#     name = models.CharField(max_length=50)
+#     price = models.IntegerField()
+#     # uploader = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+
+# class Sticker(models.Model):
+#     diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
+#     sticker_pack = models.ForeignKey("diaries.StickerPack", on_delete=CASCADE)
 
 
 # class StickerPack
