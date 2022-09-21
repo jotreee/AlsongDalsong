3276704f2ab1fd9ab129bc8aaebc1110b199ddf5
Hyoseon Kim <apso123@naver.com>
2022-09-21 13:06:59 +0900

    🌻 [BE-6-1] feat: 스티커팩 모델 생성

--- backend/diaries/models.py
+++ backend/diaries/models.py
@@ -1,4 +1,4 @@
-from tkinter import CASCADE
+from email.mime import image
 from django.db import models
 
 # Create your models here.
@@ -24,18 +24,25 @@ class Bookmark(models.Model):
     color = models.CharField(max_length=20)
 
 
-# class StickerPack(models.Model):
-#     name = models.CharField(max_length=50)
-#     price = models.IntegerField()
-#     # uploader = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
-
-# class Sticker(models.Model):
-#     diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
-#     sticker_pack = models.ForeignKey("diaries.StickerPack", on_delete=CASCADE)
+class DiaryMusic(models.Model):
+    diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
+    music = models.ForeignKey("musics.Music", on_delete=models.CASCADE)
 
 
-# class StickerPack
-# class Sticker
-# class MyStickerPack
-# class DiarySticker
-# class DiaryPlaylist
+class StickerPack(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.IntegerField()
+    # uploader = models.ForeignKey("accounts.User", on_delete=models.CASCADE)
+
+
+class Sticker(models.Model):
+    sticker_pack = models.ForeignKey("diaries.StickerPack", on_delete=models.CASCADE)
+    image_url = models.CharField(max_length=100)
+
+
+class DiarySticker(models.Model):
+    diary = models.ForeignKey("diaries.Diary", on_delete=models.CASCADE)
+    sticker = models.ForeignKey("diaries.Sticker", on_delete=models.CASCADE)
+
+# 스티커팩 구매이력(Purchase History)
+# class StickerPackPH(models.Model):
