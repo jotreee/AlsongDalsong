2f7c4595011e5bc01d58673948c856a30f77f9f3
sojung13 <bestsojung13@gmail.com>
2022-09-25 13:12:40 +0900

    🎄[FE-3-3] feat : 다이어리 API 제작

--- frontend/music-diary/src/pages/diary/DiaryItem.js
+++ frontend/music-diary/src/pages/diary/DiaryItem.js
@@ -3,34 +3,55 @@ import Card from 'react-bootstrap/Card';
 import {useNavigate} from 'react-router-dom'
 
 const DiaryItem =({...it}) => {
-    const strDate = new Date(parseInt(it.date)).toLocaleDateString();
+    const strDate = new Date(it.created_at).toLocaleDateString();
     const navigate = useNavigate();
+
+    const rightEmotion =(emotion) => {
+        if(emotion === '행복') {
+          return '/assets/img/happy_emoji.png'
+        }
+        if(emotion === '슬픔') {
+          return '/assets/img/sad_emoji.png'
+        }
+        if(emotion === '평온') {
+          return '/assets/img/normal_emoji.png'
+        }
+        if(emotion === '우울') {
+          return '/assets/img/depressed_emoji.png'
+        }
+        if(emotion === '화남') {
+          return '/assets/img/angry_emoji.png'
+        }
+        if(emotion === '놀람') {
+          return '/assets/img/anxious_emoji.png'
+        }
+      }
 
     return (<div className="diary-item" >
         {it.bookmark !==0 ? (<>
         <Card className='diary-item-card' onClick={()=>{navigate(`/diary/${it.id}`)}}>
             <Card.Body>
                 <div className='diary-item-header'>
-                    <Card.Title className='diary-item-emotion'><img src={it.emotion} style={{width:'4vw'}}></img></Card.Title>
+                    <Card.Title className='diary-item-emotion'><img src={rightEmotion(it.emotion)} style={{width:'4vw'}}></img></Card.Title>
                     <Card.Title className='diary-item-title'>{it.title}</Card.Title>
                 </div>
-                <Card.Text className='diary-item-content'>{strDate}</Card.Text>
+                <Card.Text className='diary-item-date'>{strDate}</Card.Text>
                 <Card.Text className='diary-item-content'>{it.content}</Card.Text>
-                <Card.Text className='diary-item-content'>
+                <Card.Text className='diary-item-image'>
                     <img src={it.image}></img></Card.Text>
             </Card.Body>
         </Card>
-        </>) : (<>
+        </>) : (<> 
         <Card className='diary-item-card'>
             <div className='bookmark-paper'></div>
                 <Card.Body>
                     <div className='diary-item-header'>
-                        <Card.Title className='diary-item-emotion'><img src={it.emotion} style={{width:'4vw'}}></img></Card.Title>
+                        <Card.Title className='diary-item-emotion'><img src={rightEmotion(it.emotion)} style={{width:'4vw'}}></img></Card.Title>
                         <Card.Title className='diary-item-title'>{it.title}</Card.Title>
                     </div>
-                    <Card.Text className='diary-item-content'>{strDate}</Card.Text>
+                    <Card.Text className='diary-item-date'>{strDate}</Card.Text>
                     <Card.Text className='diary-item-content'>{it.content}</Card.Text>
-                    <Card.Text className='diary-item-content'>
+                    <Card.Text className='diary-item-image'>
                     <img src={it.image}></img></Card.Text>
                 </Card.Body>
             </Card>
--- frontend/music-diary/src/pages/mainpages/Bookmarks.js
+++ frontend/music-diary/src/pages/mainpages/Bookmarks.js
@@ -3,13 +3,28 @@ import MainNote from "./MainNote";
 import {  useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryStateContext } from "../../App";
+import { getBookmarkList } from "../../api/diaryApi";
 
 import './Bookmarks.css'
 
 const Bookmarks =() => {
     const navigate = useNavigate();
     let diaryList = useContext(DiaryStateContext);
-    console.log(diaryList)
+
+    let bookmark = diaryList.filter(it=> it.bookmark === true).map(it=> it)
+
+    //  api
+  const bookmarkList = {
+    bookmark
+  }
+
+  getBookmarkList(bookmarkList)
+    .then((res)=>{
+        console.log(JSON.stringify(res.data))
+    })
+    .catch((err)=>{
+        console.log(JSON.stringify(err.data))
+    })
 
     return (
     <div className="bookmark">
