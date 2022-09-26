3079126c0154cc0a56cbc2802c0e8f693b9abd53
sskong777 <sskong777@gmail.com>
2022-09-27 01:20:42 +0900

    🍩[FE-2] Google Login
    Former-commit-id: 57f9dd38bf18e412c95bd251e732389f80905849

--- frontend/music-diary/src/components/loginpages/LoginInfoBook.js
+++ frontend/music-diary/src/components/loginpages/LoginInfoBook.js
@@ -171,7 +171,7 @@ function LoginInfoBook() {
   const scope = "https://www.googleapis.com/auth/userinfo.email"
   const GOOGLE_CLIENT_ID = "421385414738-hlk6fqfkbur8k03nuh1ftjftukoo8umd.apps.googleusercontent.com"
   // const GOOGLE_SECRET_KEY = "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"
-  GOOGLE_CALLBACK_URI = 'http://j7d204.p.ssafy.io/google/login/callback/'
+  const GOOGLE_CALLBACK_URI = 'http://j7d204.p.ssafy.io/google/login/callback/'
   const GOOGLE_AUTH_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&response_type=code&redirect_uri=${GOOGLE_CALLBACK_URI}&scope=${scope}`
 
   return (
