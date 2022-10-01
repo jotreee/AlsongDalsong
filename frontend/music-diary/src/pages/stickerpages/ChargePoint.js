2f6d31aabd3f81457a8052f1fc250fc7449ffc5a
SSAFY <sskong777@gmail.com>
2022-10-01 19:34:53 +0900

    🍩[BE-9] kakao-pay
    Former-commit-id: 9cfb88337e3bcbbc371b1b5403bb1e7d007c29a7

--- frontend/music-diary/src/pages/stickerpages/ChargePoint.js
+++ frontend/music-diary/src/pages/stickerpages/ChargePoint.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import "../../css/mypages/MySticker.css";
 import "../../css/stickerpages/ChargePoint.css";
+import axios from "axios";
 
 
 import { BiPlay, BiStore } from "react-icons/bi"; // 상점 이모지
@@ -15,25 +16,76 @@ import {
 
 import Button from "../../components/Common/Button";
 
-function ChargePoint() {
-  const navigate = useNavigate();
-  const KakaoPayBtn = () => {
-    navigate("/payment")
-  }
+// function ChargePoint() {
+//   const navigate = useNavigate();
+//   const KakaoPayBtn = () => {
+//     navigate("/payment")
+//   }
   
-  return (
+
+  class ChargePoint extends React.Component {
+    state = {
+        next_redirect_pc_url: "",
+        tid: "",
+        params: {
+            cid: "TC0ONETIME",
+            partner_order_id: "partner_order_id",
+            partner_user_id: "partner_user_id",
+            item_name: "5000 포인트충전",
+            quantity: 1,
+            total_amount: 5000,
+            vat_amount: 500,
+            tax_free_amount: 0,
+            // router에 지정한 PayResult의 경로로 수정
+            approval_url: "http://j7d204.p.ssafy.io/payresult",
+            fail_url: "http://j7d204.p.ssafy.io/payresult",
+            cancel_url: "http://j7d204.p.ssafy.io/payresult",
+        },
+    };
+
+    componentDidMount() {
+        const { params } = this.state;
+        const {total_amount} = this.state.params
+        console.log(total_amount)
+        axios({
+            url: "https://kapi.kakao.com/v1/payment/ready",
+            method: "POST",
+            headers: {
+                Authorization: "KakaoAK 1f9e18cf1db33d404f4f7cd9b5030e5a",
+                "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
+            },
+            params,
+        }).then((response) => {
+            const {
+                data: { next_redirect_pc_url, tid , total_amount},
+            } = response;
+
+            console.log(next_redirect_pc_url);
+            console.log(tid);
+            console.log(this.state.params.total_amount)
+            // localstorage에 tid 저장
+            window.localStorage.setItem("tid", tid);
+            window.localStorage.setItem("total_amount", this.state.params.total_amount);
+            this.setState({ next_redirect_pc_url, tid ,total_amount});
+        });
+    }
+
+  render() {
+    const { next_redirect_pc_url } = this.state;
+    return (
     <>
       <div className="point-charge-wrapper">
         <div className="sticker-page-header">
           포인트 충전 <BiStore />
         </div>
+        <a href={next_redirect_pc_url}>
         <img
           className="kakao-pay"
           alt="#"
           src={`${process.env.PUBLIC_URL}/assets/img/kakao-pay.jpg`}
           width="200px" height="100px"
-          onClick={KakaoPayBtn}
         />
+        </a>
         <div className="sticker-wrapper">
           <div className="header">
             <div className="header-left"></div>
@@ -48,9 +100,13 @@ function ChargePoint() {
           </div>
         </div>
       </div>      
-
     </>
-  )
+
+        // <div>
+        //     {/* <a href={next_redirect_pc_url}>{next_redirect_pc_url}</a> */}
+        // </div>
+    );
 }
+  }
 
 export default ChargePoint
