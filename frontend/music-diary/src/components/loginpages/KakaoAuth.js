85de7c231416ca95582ea8e35e9e0820f3be5c23
MilanoBeer <tgd5753@gmail.com>
2022-10-01 20:37:46 +0900

    🦁[FE] feat : 카카오 로그인 후 navigate 처리
    Former-commit-id: b01a95d8b5efb3a9cea49e5cd818c921b99bbc0e

--- frontend/music-diary/src/components/loginpages/KakaoAuth.js
+++ frontend/music-diary/src/components/loginpages/KakaoAuth.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import qs from "qs";
 import { useNavigate } from "react-router-dom";
 
+import { getUserInfoApi } from '../../api/userApi'
+
 function KakaoAuth() {
 
     const REST_API_KEY = "f742e07d1059ec8cd0050f305986a8a4"
@@ -29,14 +31,51 @@ function KakaoAuth() {
       )
       .then((res) => {
         console.log(JSON.stringify(res.data));
-        navigate("/calender");
+
+        console.log("user_id:", res.data.user.pk)
+        const user_id = res.data.user.pk; 
+
+        // 카카오 로그인한 유저 정보에서 user_id뽑아서,
+        getUserInfoApi(user_id)
+        .then((res)=>{
+          console.log("get UserInfo:", JSON.stringify(res.data))
+          console.log("sad info:", res.data.data.sad)
+          const sadEmotion = res.data.data.sad
+
+          if(sadEmotion === null){
+            console.log("설문조사 한 적 없음")
+
+            navigate('/signup/question/one')
+          }
+          else{
+            console.log("설문조사 했음")
+
+            navigate("/calender");
+          }
+          
+
+        })
+        .catch((err)=>{
+          console.log("Error")
+        })
+
+
+        // 해당 id로 유저 정보 조회,  
+        // 감정 하나 뽑아서
+        // getUserInfoApi()
+
+        // null일경우, 설문조사쪽으로 navigate
+
+        // 이미 설문조사를 했던 적이 있을 경우, 메인 캘린더로 바로 navigate
+        
+
+        
 
         // 이메일을 입력 -> 이메일로 회원정보 GET -> 
         // -> 회원정보에서 감정 라벨링상태 판단 
         // => 안되어있으면 ->, 바로 설문이동
         // 되어있으면 -> 바로 로그인,,,
         // 
-
         
         // access 토큰, refresh 토큰, user id 정보 담기
           sessionStorage.setItem("accessToken", res.data.data.token.access_token)
@@ -48,8 +87,6 @@ function KakaoAuth() {
         console.log(err.data);
       });
 
-      
-      
       // // Kakao Javascript SDK 초기화
       // if(!window.Kakao.isInitialized()){
       //   window.Kakao.init(REST_API_KEY);
@@ -58,7 +95,6 @@ function KakaoAuth() {
       // // access token 설정
       // window.Kakao.Auth.setAccessToken(res.data.access_token);
      };
-
   
   useEffect(() => {
     getToken();
